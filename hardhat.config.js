42f84f1dd692308a69239887d0270010f1b8e711
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2022-06-09 09:48:17 +0100

    package.json
    adds hardhat deploy and updates to v2.9.9

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,23 +4,9 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("hardhat-deploy"); 
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: {
     version: "0.8.6",
@@ -35,11 +21,20 @@ module.exports = {
     },
   },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    RINKEBY: {
+      url: process.env.RINKEBY_RPC_URL || "",
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      }, 
+      chainId: 4,
     },
+    hardhat: {
+    },
+  },
+  namedAccounts: {
+    deployer:{
+      default:0
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
